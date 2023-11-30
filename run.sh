1e9f73db690a177b487e5963d7372c819ddf13af
RafaelRosendof <rafael.19.rosendo@gmail.com>
2023-11-30 16:41:25 -0300

    bash script

---
+++ run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+echo "Running the Program"
+
+run="g++ sfml.cpp Hero.cpp Enemy.cpp Rocket.cpp -o sfml"
+flags="-lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio"
+
+echo "$run $flags"
+
+compile=true
+
+if [ "$compile" == "true" ]; then
+    echo "Compilando"
+    $run $flags
+
+    if [ $? -eq 0 ]; then
+        echo "Deu certo"
+        echo "Executando"
+
+        road="./sfml"
+        echo "Running $road"
+        $road
+    else
+        echo "Compilação deu Erro"
+
+    fi
+else
+    echo "Compilando pulou e deu algo a mais"
+fi
+
+
